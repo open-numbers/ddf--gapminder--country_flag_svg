0216dddb27d3a3af1d7392d8209e6c1a4e4eb975
semio <semio@posteo.net>
2021-11-26 12:34:54 +0800

    use base64

--- etl/scripts/etl_.py
+++ etl/scripts/etl_.py
@@ -17,17 +17,24 @@ def minify_svg_file(code):
     return res.replace('\n', '')
 
 
+def minify_b64_file(code):
+    path = os.path.join('../source/b64', f'{code}.svg.txt')
+    with open(path, 'r') as f:
+        res = f.read()
+    return res.replace('\n', '')
+
+
 def main():
     country_df = pd.read_csv(os.path.join(source_dir, 'ddf--entities--geo--country.csv'),
                              keep_default_na=False, na_values=[''], dtype=str)
     country_df = country_df[['country', 'iso3166_1_alpha2']].set_index('country')
-    country_df['flag_svg'] = country_df['iso3166_1_alpha2'].map(lambda x: minify_svg_file(x) if not pd.isnull(x) else x)
+    country_df['flag_svg'] = country_df['iso3166_1_alpha2'].map(lambda x: minify_b64_file(x) if not pd.isnull(x) else x)
     country_df['flag_svg'].to_csv('../../ddf--entities--geo--country.csv')
 
     geo_df = pd.read_csv(os.path.join(source_dir, 'ddf--entities--geo.csv'),
                          keep_default_na=False, na_values=[''], dtype=str)
     geo_df = geo_df[['geo', 'iso3166_1_alpha2']].set_index('geo')
-    geo_df['flag_svg'] = geo_df['iso3166_1_alpha2'].map(lambda x: minify_svg_file(x) if not pd.isnull(x) else x)
+    geo_df['flag_svg'] = geo_df['iso3166_1_alpha2'].map(lambda x: minify_b64_file(x) if not pd.isnull(x) else x)
     geo_df['flag_svg'].to_csv('../../ddf--entities--geo.csv')
 
     print('done.')
